ef069820bb307f2665f429ed003ca30fceacb008
RarchikCreation <chimmichurri.bad@gmail.com>
2025-08-23 14:01:44 +0300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from handlers.bot_instance import dp, bot
-from impl.start import init_db
+from impl.start import init_db, restore_tracking_tasks
 from utils.logger_util import logger
 from impl import start
 from utils.setup_commands import setup_bot_commands
@@ -9,6 +9,7 @@ from utils.setup_commands import setup_bot_commands
 async def main():
     logger("Bot has been launched")
     await setup_bot_commands(bot)
+    await restore_tracking_tasks()
     await dp.start_polling(bot, skip_updates=True)
 
 if __name__ == '__main__':
